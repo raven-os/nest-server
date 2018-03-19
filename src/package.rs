68b87d6193c7b2e34621626725b701a18fc94e99
Dorian Trubelle <dorian.trubelle@epitech.eu>
2018-03-19 16:32:59 +0100

    Dynamically loading the packages from the packages.toml file

--- src/package.rs
+++ src/package.rs
@@ -1,11 +1,18 @@
-#[derive(Serialize, Debug, Clone)]
+use std::fs::File;
+use std::io::Read;
+use std::error::Error;
+use std::sync::Mutex;
+
+use toml::Value;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Package {
     name: String,
     category: String,
 }
 
 impl Package {
-    pub fn new(name: &str, category: &str) -> Package {
+    pub fn new(name: &str, category: &mut str) -> Package {
         Package {
             name: name.to_string(),
             category: category.to_string(),
@@ -13,12 +20,24 @@ impl Package {
     }
 }
 
-// This is here for debugging purposes until we have a nice database
 lazy_static! {
-    pub static ref PACKAGE_LIST: Vec<Package> = vec![
-        Package::new("dash", "shell"),
-        Package::new("gcc-base", "sys-lib"),
-        Package::new("libc", "sys-lib"),
-        Package::new("libgcc", "sys-lib"),
-    ];
+    pub static ref PACKAGE_LIST: Mutex<Vec<Package>> = Mutex::new(vec![]);
+}
+
+pub fn load_packages() -> Result<(), Box<Error>> {
+    let mut file = File::open("packages.toml")?;
+        
+    let mut packages = String::new();
+    file.read_to_string(&mut packages)?;
+    
+    let toml = packages.parse::<Value>()?;
+
+    for (name, category) in toml.as_table().unwrap().iter() {
+        for (_, value) in category.as_table().unwrap().iter() {
+            let package = Package::new(&name.to_string(),
+                                       &mut value.to_string().trim_matches('"').to_string());
+            PACKAGE_LIST.lock().unwrap().push(package);
+        }
+    }
+    Ok(())
 }
--- src/routes/pull.rs
+++ src/routes/pull.rs
@@ -4,5 +4,5 @@ use package::{Package, PACKAGE_LIST};
 
 #[get("/pull")]
 fn pull() -> Json<Vec<Package>> {
-    Json(PACKAGE_LIST.clone())
+    Json(PACKAGE_LIST.lock().unwrap().clone())
 }
